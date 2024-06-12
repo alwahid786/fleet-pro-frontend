5663cb62cb6110190bd1603fc3f28310897e8640
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-06-12 09:39:49 +0500

    login page error resolved.

--- src/pages/dashboard/settings/alert/AlertType.jsx
+++ src/pages/dashboard/settings/alert/AlertType.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { Box, FormControlLabel, Switch, Typography } from "@mui/material";
 import DownloadIcon from "../../../../assets/svgs/reports/DownloadIcon";
 import EditIcon from '../../../../assets/svgs/settings/EditIcon'
+import Modal from "../../../../components/modal/Modal";
 
 const rows = [
   {
@@ -125,14 +126,16 @@ const columns = [
     headerAlign: 'center', 
     align: 'center',
     renderCell: () => (
-      <Box sx={{ 
-        display: 'inline-flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '8px',
-        cursor: 'pointer',
-        height: '100%'
-       }}>
+      <Box 
+        onClick={handleOpenEditModal}
+        sx={{ 
+          display: 'inline-flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '8px',
+          cursor: 'pointer',
+          height: '100%'
+        }}>
         <EditIcon />
       </Box>
     )
@@ -140,95 +143,112 @@ const columns = [
 ];
 
 const AlertType = () => {
+  const [modalType, setModalType] = useState(null);
+
+  const handleOpenEditModal = () => {
+    setModalType('edit');
+  }
+
+  const handleCloseModal = () => {
+    setModalType(null);
+  }
+
   return (
-    <Box
-      sx={{
-        width: "100%",
-        background: "#ffffff",
-        borderRadius: "24px 24px 0 0",
-        marginTop: "-3.5rem",
-        height: "100%",
-        overflow: "hidden",
-      }}
-    >
+    <>
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          padding: "16px",
+          width: "100%",
+          background: "#ffffff",
+          borderRadius: "24px 24px 0 0",
+          marginTop: "-3.5rem",
+          height: "100%",
+          overflow: "hidden",
         }}
       >
-        <DownloadIcon />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            padding: "16px",
+          }}
+        >
+          <DownloadIcon />
+        </Box>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5, 10, 20]}
+          headerClassName={(params) => {
+            return 'MuiDataGrid-colCell-center';
+          }}
+          cellClassName={(params) => {
+            return 'MuiDataGrid-cell-center';
+          }}
+          sx={{
+            "& .MuiDataGrid-row.even-row": {
+              backgroundColor: "#fafafa",
+            },
+            "& .MuiDataGrid-columnHeader .MuiDataGrid-columnHeaderTitle": {
+              fontSize: {
+                xs: "14px",
+                md: "16px",
+              },
+              fontWeight: 600,
+              color: "#111111",
+            },
+            "& .MuiDataGrid-row .MuiDataGrid-cell": {
+              fontSize: {
+                xs: "14px",
+                md: "16px",
+              },
+              background: "#fafafa",
+              fontWeight: 400,
+              color: "rgba(17, 17, 17, 0.6)",
+            },
+            "& .MuiDataGrid-root": {
+              borderTopLeftRadius: "24px !important",
+              borderTopRightRadius: "24px !important",
+              border: "0 !important",
+              overflow: "hidden",
+              width: "100%",
+            },
+            "& .MuiDataGrid-main": {
+              borderTopLeftRadius: "24px",
+              borderTopRightRadius: "24px",
+              width: "100%",
+              padding: "0 10px",
+            },
+            "& .MuiDataGrid-overlay": {
+              borderTopLeftRadius: "24px",
+              borderTopRightRadius: "24px",
+            },
+            "& .MuiDataGrid-footerContainer": {
+              display: "none",
+            },
+            "& .MuiDataGrid-scrollbar": {
+              "&::-webkit-scrollbar": {
+                width: "6px",
+                height: "6px",
+              },
+              "&::-webkit-scrollbar-track": {
+                background: "#00193333",
+                borderRadius: "6px",
+              },
+              "&::-webkit-scrollbar-thumb": {
+                background: "#006bce",
+                borderRadius: "10px",
+              },
+            },
+          }}
+        />
       </Box>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5, 10, 20]}
-        headerClassName={(params) => {
-          return 'MuiDataGrid-colCell-center';
-        }}
-        cellClassName={(params) => {
-          return 'MuiDataGrid-cell-center';
-        }}
-        sx={{
-          "& .MuiDataGrid-row.even-row": {
-            backgroundColor: "#fafafa",
-          },
-          "& .MuiDataGrid-columnHeader .MuiDataGrid-columnHeaderTitle": {
-            fontSize: {
-              xs: "14px",
-              md: "16px",
-            },
-            fontWeight: 600,
-            color: "#111111",
-          },
-          "& .MuiDataGrid-row .MuiDataGrid-cell": {
-            fontSize: {
-              xs: "14px",
-              md: "16px",
-            },
-            background: "#fafafa",
-            fontWeight: 400,
-            color: "rgba(17, 17, 17, 0.6)",
-          },
-          "& .MuiDataGrid-root": {
-            borderTopLeftRadius: "24px !important",
-            borderTopRightRadius: "24px !important",
-            border: "0 !important",
-            overflow: "hidden",
-            width: "100%",
-          },
-          "& .MuiDataGrid-main": {
-            borderTopLeftRadius: "24px",
-            borderTopRightRadius: "24px",
-            width: "100%",
-            padding: "0 10px",
-          },
-          "& .MuiDataGrid-overlay": {
-            borderTopLeftRadius: "24px",
-            borderTopRightRadius: "24px",
-          },
-          "& .MuiDataGrid-footerContainer": {
-            display: "none",
-          },
-          "& .MuiDataGrid-scrollbar": {
-            "&::-webkit-scrollbar": {
-              width: "6px",
-              height: "6px",
-            },
-            "&::-webkit-scrollbar-track": {
-              background: "#00193333",
-              borderRadius: "6px",
-            },
-            "&::-webkit-scrollbar-thumb": {
-              background: "#006bce",
-              borderRadius: "10px",
-            },
-          },
-        }}
-      />
-    </Box>
+      {modalType === 'edit' && (
+        <Modal onClose={handleCloseModal}>
+          <EditUser onClose={handleCloseModal} />
+        </Modal>
+      )}
+    </>
   );
 };
 
---
+++ src/pages/dashboard/settings/alert/components/EditAlert.jsx
@@ -0,0 +1,102 @@
+import { Box, Button, Grid, styled, MenuItem, TextField, Typography } from '@mui/material'
+import React from 'react'
+import BackIcon from '../../../../../assets/svgs/modal/BackIcon'
+import CloseIcon from '../../../../../assets/svgs/modal/CloseIcon'
+import { roles } from '../../../../../data/data'
+import { regions } from '../../../../../data/data'
+
+const EditAlert = ({ onClose, label, maxLength, type }) => {
+
+  return (
+    <>
+        <Box sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}>
+             <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '1rem',
+                color: 'rgba(17, 17, 17, 1)',
+                fontSize: {
+                    xs: '1rem',
+                    md: '1.5rem'
+                },
+                fontWeight: 600
+             }}>
+                <Box sx={{cursor: 'pointer', height: '25px'}} onClick={onClose}>
+                    <BackIcon />
+                </Box>
+                EDIT USER
+             </Box>
+             <Box sx={{cursor: 'pointer'}} onClick={onClose}>
+                <CloseIcon onClick={onClose} />
+            </Box>  
+        </Box>
+        {/* Form  */}
+        <Box sx={{
+            marginTop: {
+                xs: '1rem',
+                lg: '2.5rem',
+            }
+        }}>
+            <Grid container spacing='16'>
+                <Grid item xs='12' lg='6'>
+                    <TextField 
+                        select
+                        fullWidth
+                        label='Role'
+                    >
+                        {roles.map((role, i) => (
+                            <MenuItem key={i} value={role.role}>
+                                {role.role}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <TextField 
+                        select
+                        fullWidth
+                        label='Region'
+                        sx={{ marginTop: '1rem' }}
+                    >
+                        {regions.map((region, i) => (
+                            <MenuItem key={i} value={region.region}>
+                                {region.region}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Grid>
+                <Grid item xs='12' lg='6' display='flex' flexDirection='column' alignItems='flex-end'>
+                    <Box sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '1rem',
+                        width: '255px',
+                        justifyContent: 'center'
+                    }}>
+                        <CancelBtn onClick={onClose}>Cancel</CancelBtn>
+                        <Button sx={{
+                            color: '#fff',
+                            borderRadius: '16px',
+                            width: '137px',
+                            padding: '16px'
+                        }}>
+                            Save
+                        </Button>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    </>
+  )
+}
+
+export default EditAlert
+
+const CancelBtn = styled('span')({
+    fontsize: '16px',
+    fontWeight: 600,
+    color: 'rgba(17, 17, 17, 1)',
+    cursor: 'pointer'  
+})
