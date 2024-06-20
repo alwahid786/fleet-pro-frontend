71568f7fcd2a6e9d616aa01cc86d5848ac663ab4
Wahid Ahmad <120777629+alwahid786@users.noreply.github.com>
2024-06-20 20:06:13 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: 'dist'
-  }
-});
+  plugins: [react()],
+})
