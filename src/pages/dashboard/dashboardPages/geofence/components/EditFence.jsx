d93a50c00b00a2138b80abe71f3540b2d7a5db8f
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-07-12 10:22:19 +0500

    add geofence data

--- src/pages/dashboard/dashboardPages/geofence/components/EditFence.jsx
+++ src/pages/dashboard/dashboardPages/geofence/components/EditFence.jsx
@@ -1,14 +1,6 @@
-import {
-  Box,
-  FormControl,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-  Typography,
-} from "@mui/material";
-import React, { useEffect, useState } from "react";
+/* eslint-disable react/prop-types */
+import { Box, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
+import { Fragment, useEffect, useState } from "react";
 import BackIcon from "../../../../../assets/svgs/modal/BackIcon";
 import CloseIcon from "../../../../../assets/svgs/modal/CloseIcon";
 import EditMap from "./EditMap";
@@ -17,169 +9,154 @@ import { useDispatch, useSelector } from "react-redux";
 import { getSingleGeofenceAction } from "../../../../../redux/actions/geofence.action";
 
 const EditFence = ({ onClose, editSelectedRow }) => {
-  const dispatch=useDispatch()
-  const {geofence}=useSelector((state)=>state.geofence)
-  const [name, setName] = useState(geofence?.name );
-  const [alert, setAlert] = useState(geofence?.alert);
-  const [status, setStatus] = useState(geofence?.status);
-  const [startDate, setStartDate] = useState(geofence?.startDate?.split("T")[0]);
-  const [endDate, setEndDate] = useState(geofence?.endDate?.split("T")[0]);
-  const [trucks,setTrucks]=useState(geofence?.trucks)
+    const dispatch = useDispatch();
+    const { geofence } = useSelector((state) => state.geofence);
+    const [name, setName] = useState(geofence?.name || "");
+    const [alert, setAlert] = useState(geofence?.alert || "");
+    const [status, setStatus] = useState(geofence?.status || "");
+    const [startDate, setStartDate] = useState(geofence?.startDate?.split("T")[0] || "");
+    const [endDate, setEndDate] = useState(geofence?.endDate?.split("T")[0] || "");
+    const [trucks, setTrucks] = useState(geofence?.trucks || []);
 
+    useEffect(() => {
+        dispatch(getSingleGeofenceAction(editSelectedRow?._id));
+    }, [dispatch, editSelectedRow?._id]);
 
-  useEffect(() => {
-    dispatch(getSingleGeofenceAction(editSelectedRow?._id))
-  }, [editSelectedRow?._id])
-  useEffect(() => {
-    if(geofence){
-      setName(geofence?.name)
-      setAlert(geofence?.alert)
-      setStatus(geofence?.status)
-      setStartDate(geofence?.startDate)
-      setEndDate(geofence?.endDate)
-      setTrucks(geofence?.trucks||[])
-    }
-  }, [editSelectedRow,dispatch,geofence])
-  
-  return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            gap: "1rem",
-            color: "rgba(17, 17, 17, 1)",
-            fontSize: {
-              xs: "1rem",
-              md: "1.5rem",
-            },
-            fontWeight: 600,
-          }}
-        >
-          <Box sx={{ cursor: "pointer", height: "25px" }} onClick={onClose}>
-            <BackIcon />
-          </Box>
-          EDIT FENCE
-        </Box>
-        <Box sx={{ cursor: "pointer" }} onClick={onClose}>
-          <CloseIcon onClick={onClose} />
-        </Box>
-      </Box>
-      <Typography
-        variant="h3"
-        sx={{ color: "#000", fontSize: "20px", fontWeight: 700 }}
-      >
-        Info
-      </Typography>
-      <Grid container spacing="14" mt={1}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            fullWidth
-            type="text"
-            label="Geofence Name"
-            name="geofenceName"
-            value={name}
-            onChange={(e)=>setName(e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <FormControl fullWidth>
-            <InputLabel id="select-status">Select Status</InputLabel>
-            <Select
-              labelId="select-status"
-              label="Select Status"
-              name="status"
-              value={status}
-            onChange={(e)=>setStatus(e.target.value)}
+    useEffect(() => {
+        if (geofence) {
+            setName(geofence?.name);
+            setAlert(geofence?.alert);
+            setStatus(geofence?.status);
+            setStartDate(geofence?.startDate);
+            setEndDate(geofence?.endDate);
+            setTrucks(geofence?.trucks || []);
+        }
+    }, [editSelectedRow, dispatch, geofence]);
 
-              sx={{ width: "100%" }}
+    return (
+        <Fragment>
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                }}
             >
-              <MenuItem value={"inactive"}>Inactive</MenuItem>
-              <MenuItem value={"active"}>Active</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <FormControl fullWidth>
-            <InputLabel id="select-fence">Select Fence</InputLabel>
-            <Select
-              labelId="select-fence"
-              label="Select Fence"
-              name="alert"
-              value={alert}
-            onChange={(e)=>setAlert(e.target.value)}
-
-              sx={{ width: "100%" }}
-            >
-              <MenuItem value={"infence"}>In-Fence</MenuItem>
-              <MenuItem value={"outfence"}>Out-Fence</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs="12" lg="6">
-          <TextField
-            type="date"
-            label="startDate"
-            maxLength="30"
-            fullWidth
-            value={new Date(startDate)}
-            onChange={(e)=>setStartDate(e.target.value)}
-
-            name="startDate"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-        </Grid>
-        <Grid item xs="12" lg="6">
-          <TextField
-            type="date"
-            label="endDate"
-            maxLength="30"
-            fullWidth
-            value={new Date(endDate)}
-            onChange={(e)=>setEndDate(e.target.value)}
-
-            name="endDate"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-        </Grid>
-      </Grid>
-      {/* Map */}
-      <Typography
-        mt={2}
-        variant="h3"
-        sx={{ color: "#000", fontSize: "20px", fontWeight: 700 }}
-      >
-        Location
-      </Typography>
-      <Box mt={2} sx={{ width: "100%", height: "400px" }}>
-        <EditMap />
-      </Box>
-      {/* List */}
-      <Typography
-        mt={2}
-        variant="h3"
-        sx={{ color: "#000", fontSize: "20px", fontWeight: 700 }}
-      >
-        List
-      </Typography>
-      <Grid container mt={2}>
-        <Grid items xs={12} md={12}>
-          <TruckList geofenceId={geofence?._id} trucks={trucks} />
-        </Grid>
-      </Grid>
-    </>
-  );
+                <Box
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "1rem",
+                        color: "rgba(17, 17, 17, 1)",
+                        fontSize: {
+                            xs: "1rem",
+                            md: "1.5rem",
+                        },
+                        fontWeight: 600,
+                    }}
+                >
+                    <Box sx={{ cursor: "pointer", height: "25px" }} onClick={onClose}>
+                        <BackIcon />
+                    </Box>
+                    EDIT FENCE
+                </Box>
+                <Box sx={{ cursor: "pointer" }} onClick={onClose}>
+                    <CloseIcon onClick={onClose} />
+                </Box>
+            </Box>
+            <Typography variant="h3" sx={{ color: "#000", fontSize: "20px", fontWeight: 700 }}>
+                Info
+            </Typography>
+            <Grid container spacing="14" mt={1}>
+                <Grid item xs={12} md={6}>
+                    <TextField
+                        fullWidth
+                        type="text"
+                        label="Geofence Name"
+                        name="geofenceName"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <FormControl fullWidth>
+                        <InputLabel id="select-status">Select Status</InputLabel>
+                        <Select
+                            labelId="select-status"
+                            label="Select Status"
+                            name="status"
+                            value={status}
+                            onChange={(e) => setStatus(e.target.value)}
+                            sx={{ width: "100%" }}
+                        >
+                            <MenuItem value={"inactive"}>Inactive</MenuItem>
+                            <MenuItem value={"active"}>Active</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <FormControl fullWidth>
+                        <InputLabel id="select-fence">Select Fence</InputLabel>
+                        <Select
+                            labelId="select-fence"
+                            label="Select Fence"
+                            name="alert"
+                            value={alert}
+                            onChange={(e) => setAlert(e.target.value)}
+                            sx={{ width: "100%" }}
+                        >
+                            <MenuItem value={"infence"}>In-Fence</MenuItem>
+                            <MenuItem value={"outfence"}>Out-Fence</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item xs="12" lg="6">
+                    <TextField
+                        type="date"
+                        label="startDate"
+                        maxLength="30"
+                        fullWidth
+                        value={new Date(startDate)}
+                        onChange={(e) => setStartDate(e.target.value)}
+                        name="startDate"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </Grid>
+                <Grid item xs="12" lg="6">
+                    <TextField
+                        type="date"
+                        label="endDate"
+                        maxLength="30"
+                        fullWidth
+                        value={new Date(endDate)}
+                        onChange={(e) => setEndDate(e.target.value)}
+                        name="endDate"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </Grid>
+            </Grid>
+            {/* Map */}
+            <Typography mt={2} variant="h3" sx={{ color: "#000", fontSize: "20px", fontWeight: 700 }}>
+                Location
+            </Typography>
+            <Box mt={2} sx={{ width: "100%", height: "400px" }}>
+                <EditMap />
+            </Box>
+            {/* List */}
+            <Typography mt={2} variant="h3" sx={{ color: "#000", fontSize: "20px", fontWeight: 700 }}>
+                List
+            </Typography>
+            <Grid container mt={2}>
+                <Grid items xs={12} md={12}>
+                    <TruckList geofenceId={geofence?._id} trucks={trucks} />
+                </Grid>
+            </Grid>
+        </Fragment>
+    );
 };
 
 export default EditFence;
--- src/pages/dashboard/dashboardPages/geofence/components/TruckList.jsx
+++ src/pages/dashboard/dashboardPages/geofence/components/TruckList.jsx
@@ -1,137 +1,156 @@
+/* eslint-disable react/prop-types */
+import { Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import DeleteIcon from "../../../../../assets/svgs/geofence/DeleteIcon";
-import { Box } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
 import {
-  getAllGeofenceAction,
-  removeTruckFromGeofenceAction,
+    getSingleGeofenceAction,
+    removeTruckFromGeofenceAction,
 } from "../../../../../redux/actions/geofence.action";
+import { toast } from "react-toastify";
+import { confirmAlert } from "react-confirm-alert";
 
 const TruckList = ({ trucks, geofenceId }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const dispatch = useDispatch();
+    const [isLoading, setIsLoading] = useState(false);
+    const dispatch = useDispatch();
 
+    const handleDeleteList = async (row) => {
+        confirmAlert({
+            title: "Confirm Remove",
+            message: "Are you sure you want to Remove this truck?",
+            buttons: [
+                {
+                    label: "Yes",
+                    onClick: async () => {
+                        if (row?.id) {
+                            setIsLoading(true);
+                            await dispatch(removeTruckFromGeofenceAction(geofenceId, row._id));
+                            await dispatch(getSingleGeofenceAction(geofenceId));
+                            setIsLoading(false);
+                        }
+                    },
+                },
+                {
+                    label: "No",
+                    onClick: () => toast.info("Delete action cancelled", { autoClose: 2000 }),
+                },
+            ],
+        });
+    };
 
-  const handleDeleteList = async (row) => {
-    setIsLoading(true);
-    await dispatch(removeTruckFromGeofenceAction(geofenceId, row._id));
-    await dispatch(getAllGeofenceAction());
-    setIsLoading(false);
-  };
-
-  const columns = [
-    {
-      field: "truckName",
-      headerName: " TRUCK NAME",
-      headerAlign: "center",
-      align: "center",
-      width: 200,
-    },
-    {
-      field: "fleetNumber",
-      headerName: "FLEET NUMBER",
-      headerAlign: "center",
-      align: "center",
-      width: 200,
-    },
-    {
-      field: "status",
-      headerName: "STATUS",
-      headerAlign: "center",
-      align: "center",
-      width: 200,
-    },
-    {
-      field: "plateNumber",
-      headerName: "PLATE NUMBER",
-      headerAlign: "center",
-      align: "center",
-      width: 200,
-    },
-    {
-      field: "delete",
-      headerName: "DELETE",
-      headerAlign: "center",
-      align: "center",
-      width: 100,
-      renderCell: (params) => (
-        <Box
-          sx={{ cursor: isLoading ? "not-allowed" : "pointer" }}
-          onClick={() => handleDeleteList(params.row)}
-        >
-          <DeleteIcon />
-        </Box>
-      ),
-    },
-  ];
-  return (
-    <>
-      <DataGrid
-        rows={trucks}
-        columns={columns}
-        getRowId={(row) => row._id}
-        pageSize={5}
-        rowsPerPageOptions={[5, 10, 20]}
-        sx={{
-          "& .MuiDataGrid-row.even-row": {
-            backgroundColor: "#fafafa",
-          },
-          "& .MuiDataGrid-columnHeader .MuiDataGrid-columnHeaderTitle": {
-            fontSize: {
-              xs: "12px",
-              md: "13px",
-            },
-            fontWeight: 600,
-            color: "#111111",
-          },
-          "& .MuiDataGrid-row .MuiDataGrid-cell": {
-            fontSize: {
-              xs: "12px",
-              md: "13px",
-            },
-            background: "#fafafa",
-            fontWeight: 400,
-            color: "rgba(17, 17, 17, 0.6)",
-          },
-          "& .MuiDataGrid-root": {
-            borderTopLeftRadius: "24px !important",
-            borderTopRightRadius: "24px !important",
-            border: "0 !important",
-            overflow: "hidden",
-            width: "100%",
-          },
-          "& .MuiDataGrid-main": {
-            borderTopLeftRadius: "24px",
-            borderTopRightRadius: "24px",
-            width: "100%",
-            padding: "0px",
-          },
-          "& .MuiDataGrid-overlay": {
-            borderTopLeftRadius: "24px",
-            borderTopRightRadius: "24px",
-          },
-          "& .MuiDataGrid-footerContainer": {
-            display: "none",
-          },
-          "& .MuiDataGrid-scrollbar": {
-            "&::-webkit-scrollbar": {
-              width: "6px",
-              height: "6px",
-            },
-            "&::-webkit-scrollbar-track": {
-              background: "#00193333",
-              borderRadius: "6px",
-            },
-            "&::-webkit-scrollbar-thumb": {
-              background: "#006bce",
-              borderRadius: "10px",
-            },
-          },
-        }}
-      />
-    </>
-  );
+    const columns = [
+        {
+            field: "truckName",
+            headerName: " TRUCK NAME",
+            headerAlign: "center",
+            align: "center",
+            width: 200,
+        },
+        {
+            field: "fleetNumber",
+            headerName: "FLEET NUMBER",
+            headerAlign: "center",
+            align: "center",
+            width: 200,
+        },
+        {
+            field: "status",
+            headerName: "STATUS",
+            headerAlign: "center",
+            align: "center",
+            width: 200,
+        },
+        {
+            field: "plateNumber",
+            headerName: "PLATE NUMBER",
+            headerAlign: "center",
+            align: "center",
+            width: 200,
+        },
+        {
+            field: "delete",
+            headerName: "DELETE",
+            headerAlign: "center",
+            align: "center",
+            width: 100,
+            renderCell: (params) => (
+                <Box
+                    sx={{ cursor: isLoading ? "not-allowed" : "pointer" }}
+                    onClick={() => handleDeleteList(params.row)}
+                >
+                    <DeleteIcon />
+                </Box>
+            ),
+        },
+    ];
+    return (
+        <React.Fragment>
+            <DataGrid
+                rows={trucks}
+                columns={columns}
+                getRowId={(row) => row._id}
+                pageSize={5}
+                rowsPerPageOptions={[5, 10, 20]}
+                sx={{
+                    "& .MuiDataGrid-row.even-row": {
+                        backgroundColor: "#fafafa",
+                    },
+                    "& .MuiDataGrid-columnHeader .MuiDataGrid-columnHeaderTitle": {
+                        fontSize: {
+                            xs: "12px",
+                            md: "13px",
+                        },
+                        fontWeight: 600,
+                        color: "#111111",
+                    },
+                    "& .MuiDataGrid-row .MuiDataGrid-cell": {
+                        fontSize: {
+                            xs: "12px",
+                            md: "13px",
+                        },
+                        background: "#fafafa",
+                        fontWeight: 400,
+                        color: "rgba(17, 17, 17, 0.6)",
+                    },
+                    "& .MuiDataGrid-root": {
+                        borderTopLeftRadius: "24px !important",
+                        borderTopRightRadius: "24px !important",
+                        border: "0 !important",
+                        overflow: "hidden",
+                        width: "100%",
+                    },
+                    "& .MuiDataGrid-main": {
+                        borderTopLeftRadius: "24px",
+                        borderTopRightRadius: "24px",
+                        width: "100%",
+                        padding: "0px",
+                    },
+                    "& .MuiDataGrid-overlay": {
+                        borderTopLeftRadius: "24px",
+                        borderTopRightRadius: "24px",
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        display: "none",
+                    },
+                    "& .MuiDataGrid-scrollbar": {
+                        "&::-webkit-scrollbar": {
+                            width: "6px",
+                            height: "6px",
+                        },
+                        "&::-webkit-scrollbar-track": {
+                            background: "#00193333",
+                            borderRadius: "6px",
+                        },
+                        "&::-webkit-scrollbar-thumb": {
+                            background: "#006bce",
+                            borderRadius: "10px",
+                        },
+                    },
+                }}
+            />
+        </React.Fragment>
+    );
 };
 
 export default TruckList;
